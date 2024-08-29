91ad0b561a38dfb718edfa3bdf4f7a8d22ad385e
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-29 16:12:14 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ const App = () => {
   const[arr] = useState(["Satish", "Ankita", "Amit", "Chetan", "Vijay", "Rahul"]);
   return (
     <div id="main">
-               <ol key="relativeList">
+               <ol key="relativeList" id='relativeList'>
                 {
-                  arr.map((item, index) => <li key={'relativeListItem'+(index+1)}>{item}</li>)
+                  arr.map((item, index) => <li key={'relativeListItem'+(index+1)} id={'relativeListItem'+(index+1)}>{item}</li>)
                 }
                </ol>
     </div>
